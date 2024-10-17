f536e9ea1e374f1f9c85d47215c07ef0a5af5266
Eric Silva <125579424+erictsilva@users.noreply.github.com>
2024-10-17 20:30:45 -0300

    Exercicio5.java

--- Exercicio5.java
+++ Exercicio5.java
@@ -2,25 +2,27 @@ import java.util.LinkedList;
 import java.util.List;
 
 public class Exercicio5 {
-    private int numVertices; 
-    private List<List<Integer>> listaAdj; 
+    
+    static class GrafoListaAdj {
+    private int vertices; 
+    private List<Integer>[] listaAdj;  
 
-    public Exercicio5(int numVertices) {
-        this.numVertices = numVertices;
-        listaAdj = new LinkedList<>(); 
+    public GrafoListaAdj(int vertices) {
+        this.vertices = vertices;
+        listaAdj = new LinkedList[vertices];
 
-        for (int i = 0; i < numVertices; i++) {
-            listaAdj.add(new LinkedList<>());
+        for (int i = 0; i < vertices; i++) {
+            listaAdj[i] = new LinkedList<>();
         }
     }
     public void adicionarAresta(int origem, int destino) {
-        listaAdj.get(origem).add(destino); 
-        listaAdj.get(destino).add(origem); 
+        listaAdj[origem].add(destino); 
+        listaAdj[destino].add(origem);  
     }
     public void mostrarGrafo() {
-        for (int i = 0; i < numVertices; i++) {
+        for (int i = 0; i < vertices; i++) {
             System.out.print("Vértice " + i + ":");
-            for (Integer adj : listaAdj.get(i)) {
+            for (Integer adj : listaAdj[i]) {
                 System.out.print(" -> " + adj);
             }
             System.out.println();
@@ -28,8 +30,9 @@ public class Exercicio5 {
     }
 
     public static void main(String[] args) {
-        Exercicio5 grafo = new Exercicio5(5);
-        
+  
+        GrafoListaAdj grafo = new GrafoListaAdj(5);
+
         grafo.adicionarAresta(0, 1);
         grafo.adicionarAresta(0, 4);
         grafo.adicionarAresta(1, 2);
@@ -40,4 +43,5 @@ public class Exercicio5 {
 
         grafo.mostrarGrafo();
     }
+}
 }
