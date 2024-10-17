74f76d04a3d750b1cb9858dbb0346b9e019dc336
Eric Silva <125579424+erictsilva@users.noreply.github.com>
2024-10-17 18:31:14 -0300

    Exercicio3.java

--- Exercicio3.java
+++ Exercicio3.java
@@ -1,23 +1,37 @@
+import java.util.Scanner;
+
 public class Exercicio3 {
-    public static int search(int[] array, int posicao) {
+
+    public static int buscaLinear(int[] array, int numeroProcurado) {
+       
         for (int i = 0; i < array.length; i++) {
-            if (array[i] == posicao) {
-                return i;
+          
+            if (array[i] == numeroProcurado) {
+                return i;  
             }
         }
-        return -1;
+        return -1;  
     }
-
     public static void main(String[] args) {
-        int[] array = {19, 21, 65, 20, 1, 78};
-        int posicao = 12;
+        Scanner scanner = new Scanner(System.in);
+
+        int[] array = {10, 23, 45, 78, 92, 100, 56, 38 , 45, 94, 85, 21, 36};
+        int posicao;
+
+        do {
         
-        int resultado = search(array, posicao);
-        
-        if (resultado != -1) {
-            System.out.println("O número " + posicao + " foi encontrado na posição: " + resultado);
-        } else {
-            System.out.println("O número " + posicao + " não encontrado.");
-        }
+            System.out.println("Digite o número que deseja procurar: ");
+            int numeroProcurado = scanner.nextInt();
+
+            posicao = buscaLinear(array, numeroProcurado);
+
+            if (posicao != -1) {
+                System.out.print("Número ' " + numeroProcurado + " ' encontrado na posição: " + posicao);
+            } else {
+                System.out.println("Número ' " + numeroProcurado + " ' não encontrado. Tente novamente.");
+            }
+        } while (posicao == -1);  
+
+        scanner.close();
     }
 }
